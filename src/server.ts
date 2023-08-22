97fd35f9405fdcc6bd6968de676e4e177e7f1347
Pedro Gonzaga <pedro.gonzaga@deucredito.com.br>
2023-08-22 15:12:25 -0300

    video 7 - prettier

--- src/server.ts
+++ src/server.ts
@@ -4,29 +4,27 @@ import { Application } from 'express';
 import bodyParser from 'body-parser';
 import { ForecastController } from './util/controllers/forecast';
 
-
 export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
 
-    constructor(private port = 3000) {
-        super();
-    }
+  public async init(): Promise<void> {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-    public async init(): Promise<void> {
-        this.setupExpress();
-        this.setupControllers();
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+    this.setupControllers();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-        this.setupControllers();
-    }
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    private setupControllers(): void {
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
-    }
-
-    public getApp(): Application {
-        return this.app;
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
